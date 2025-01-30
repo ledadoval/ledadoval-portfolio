a1ce739982a69a12c943a52682722d3ea1676679
Leda <ledadoval2002@gmail.com>
2025-01-30 14:34:21 -0300

    add links

--- ledadoval/src/components/skills.js
+++ ledadoval/src/components/skills.js
@@ -8,17 +8,17 @@ function Skills(){
         <h2>my skills</h2>
         <h5>list of tools that I already use, or study on my projects</h5>
         <div className='skills-buttons'>
-        <button>html</button>
-        <button>css</button>
-        <button>javascript</button>
-        <button>react</button>
-        <button>react native</button>
-        <button>node js</button>
-        <button>java</button>
-        <button>c#</button>
-        <button>mysql</button>
-        <button>figma</button>
-        <button>git</button>
+        <button onClick={() => window.open('https://developer.mozilla.org/en-US/docs/Glossary/HTML5')}>html</button>
+        <button onClick={() => window.open('https://developer.mozilla.org/en-US/docs/Web/CSS')}>css</button>
+        <button onClick={() => window.open('https://developer.mozilla.org/en-US/docs/Web/JavaScript')}>javascript</button>
+        <button onClick={() => window.open('https://react.dev/')}>react</button>
+        <button onClick={() => window.open('https://reactnative.dev/')}>react native</button>
+        <button onClick={() => window.open('https://nodejs.org/pt')}>node js</button>
+        <button onClick={() => window.open('https://dev.java/')}>java</button>
+        <button onClick={() => window.open('https://learn.microsoft.com/en-us/dotnet/csharp/')}>c#</button>
+        <button onClick={() => window.open('https://dev.mysql.com/doc/')}>mysql</button>
+        <button onClick={() => window.open('https://help.figma.com/hc/en-us')}>figma</button>
+        <button onClick={() => window.open('https://git-scm.com/doc')}>git</button>
         </div>
         </div>
 
