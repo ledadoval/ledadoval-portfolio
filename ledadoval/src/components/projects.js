e76abed56a3de342e3b54823ea927d3b18b88d53
Leda <ledadoval2002@gmail.com>
2025-02-06 15:47:14 -0300

    add cards effects

--- ledadoval/src/components/projects.js
+++ ledadoval/src/components/projects.js
@@ -6,31 +6,31 @@ function Projects() {
       title: "communibus",
       description: "a web system developed in the 3rd semester of the Systems Analysis and Development course, helping users find the nearest bus schedules based on their geolocation.",
       repolink: "https://github.com/ICEI-PUC-Minas-PSG-ADS-TI/TIAPLI-2023-2-CommuniBus",
-      color: " #4b0101",
+      color: " white",
     },
     {
       title: "locaveiculos",
       description: "an API developed in C# during the 4th semester of the Systems Analysis and Development course.",
       repolink: "https://github.com/PSG-TADS/psg-tads-2024-1-back-bd-ledadoval",
-      color: " #4b0101",
+      color: " white",
     },
     {
       title: "plantbuddy",
       description: "a project developed in React Native to provide users with all the necessary information about plant cultivation, developed in the 5th semester of the Systems Analysis and Development course.",
       repolink: "#",
-      color: " #4b0101",
+      color: " white",
     },
     {
       title: "lrburguer",
       description: "a landing page built using HTML, CSS, and JavaScript.",
       repolink: "#",
-      color: " #4b0101",
+      color: " white",
     },
     {
       title: "atorf",
       description: "an OOP project developed in Java for vehicle fleet management.",
       repolink: "#",
-      color: " #4b0101",
+      color: " white",
     },
     {
       title: "excusify",
@@ -56,7 +56,8 @@ function Projects() {
             className="project-card"
             style={{  borderColor: project.color,
               borderStyle: 'solid',
-              borderWidth: '2px'}}
+              borderWidth: '2px',
+            }}
           >
             <h3 style={{ color: project.color }}>{project.title}</h3>
             <p>{project.description}</p>
