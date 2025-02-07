2b6445e81602861c58127b84f76308fcc5ae33cb
Leda <ledadoval2002@gmail.com>
2025-02-06 21:08:24 -0300

    changing the icons

--- ledadoval/src/components/aboutme.js
+++ ledadoval/src/components/aboutme.js
@@ -10,9 +10,7 @@ function AboutMe() {
                 My first contact with coding came through elective courses, and I soon realized it was the path I wanted to pursue. 
                 Throughout my studies, I explored different areas and developed a strong interest in frontend development. 
                 With experience in marketing, I also understand and apply SEO techniques to enhance digital projects. Now, I am 
-                expanding my skills in data science and machine learning.
-                
-                
+                expanding my skills in data science and machine learning.                
             <div className='button-aboutme'>
                 <a href='#skills'>
                     <button>check my skills</button>
--- ledadoval/src/components/skills.js
+++ ledadoval/src/components/skills.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import '../App.css';
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaGitAlt, FaWordpress, FaJava, FaPython } from 'react-icons/fa';
+import { SiMysql, SiCsharp, SiFigma, SiPowerbi } from 'react-icons/si';
+import { SiDotnet } from 'react-icons/si'; // Exemplo de substituição para C#
+import { SiMicrosoftpowerpoint } from 'react-icons/si'; // Caso não encontre Power BI, use algo próximo
+
 
 function Skills(){
     return(
@@ -7,20 +12,47 @@ function Skills(){
         <h2 className='skills-h2'>my skills</h2>
         <h5>list of tools that I already use, or study on my projects</h5>
         <div className='skills-buttons'>
-        <button onClick={() => window.open('https://developer.mozilla.org/en-US/docs/Glossary/HTML5')}>html</button>
-        <button onClick={() => window.open('https://developer.mozilla.org/en-US/docs/Web/CSS')}>css</button>
-        <button onClick={() => window.open('https://developer.mozilla.org/en-US/docs/Web/JavaScript')}>javascript</button>
-        <button onClick={() => window.open('https://react.dev/')}>react</button>
-        <button onClick={() => window.open('https://reactnative.dev/')}>react native</button>
-        <button onClick={() => window.open('https://nodejs.org/pt')}>node js</button>
-        <button onClick={() => window.open('https://learn.microsoft.com/en-us/dotnet/csharp/')}>c#</button>
-        <button onClick={() => window.open('https://dev.mysql.com/doc/')}>mysql</button>
-        <button onClick={() => window.open('https://help.figma.com/hc/en-us')}>figma</button>
-        <button onClick={() => window.open('https://help.figma.com/hc/en-us')}>wordpress</button>
-        <button onClick={() => window.open('https://git-scm.com/doc')}>git</button>
-        <button onClick={() => window.open('https://dev.java/')}>java</button>
-        <button onClick={() => window.open('https://learn.microsoft.com/en-us/power-bi/')}>power bi</button>
-        <button onClick={() => window.open('https://www.python.org/doc/')}>python</button>
+        <button onClick={() => window.open('https://developer.mozilla.org/en-US/docs/Glossary/HTML5')}>
+          <FaHtml5 size={32} color="#e34c26" />
+        </button>
+        <button onClick={() => window.open('https://developer.mozilla.org/en-US/docs/Web/CSS')}>
+          <FaCss3Alt size={32} color="#1572b6" />
+        </button>
+        <button onClick={() => window.open('https://developer.mozilla.org/en-US/docs/Web/JavaScript')}>
+          <FaJs size={32} color="#f7df1e" />
+        </button>
+        <button onClick={() => window.open('https://react.dev/')}>
+          <FaReact size={32} color="#61dafb" />
+        </button>
+        <button onClick={() => window.open('https://reactnative.dev/')}>
+          <FaReact size={32} color="#61dafb" />
+        </button>
+        <button onClick={() => window.open('https://nodejs.org/pt')}>
+          <FaNodeJs size={32} color="#339933" />
+        </button>
+        <button onClick={() => window.open('https://learn.microsoft.com/en-us/dotnet/csharp/')}>
+          <SiDotnet size={32} color="#239120" />
+        </button>
+        <button onClick={() => window.open('https://dev.mysql.com/doc/')}>
+          <SiMysql size={32} color="#4479a1" />
+        </button>
+        <button onClick={() => window.open('https://help.figma.com/hc/en-us')}>
+          <SiFigma size={32} color="#f24e1e" />
+        </button>
+        <button onClick={() => window.open('https://wordpress.com/')}>
+          <FaWordpress size={32} color="#21759b" />
+        </button>
+        <button onClick={() => window.open('https://git-scm.com/doc')}>
+          <FaGitAlt size={32} color="#f05032" />
+        </button>
+        <button onClick={() => window.open('https://dev.java/')}>
+          <FaJava size={32} color="#007396" />
+        </button>
+        <button onClick={() => window.open('https://learn.microsoft.com/en-us/power-bi/')}>
+        </button>
+        <button onClick={() => window.open('https://www.python.org/doc/')}>
+          <FaPython size={32} color="#3776ab" />
+        </button>
         </div>
         </div>
 
